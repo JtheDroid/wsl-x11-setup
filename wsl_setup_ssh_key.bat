262e90ebc2763e0f30429d61d872b4816801bc9e
JtheDroid <fabi.jobo@gmail.com>
2021-01-06 01:04:42 +0100

    ssh setup: Add host key to known_hosts

--- wsl_setup_ssh_key.bat
+++ wsl_setup_ssh_key.bat
@@ -1,8 +1,13 @@
-
-if not defined distroarg wsl_set_distro.bat
+@echo off
+if not defined distroarg call wsl_set_distro.bat
 setlocal
 wsl %distroarg% mkdir -p $HOME/.ssh
-set pubkeypath=%userprofile%\.ssh\id_rsb.pub
-IF NOT EXIST "%pubkeypath%" ssh-keygen
-IF EXIST "%pubkeypath%" type "%pubkeypath%" | wsl %distroarg% -- cat - ^>^> $HOME/.ssh/authorized_keys
+set pubkeypath=%userprofile%\.ssh\id_rsa.pub
+if not exist "%pubkeypath%" ssh-keygen
+if exist "%pubkeypath%" type "%pubkeypath%" | wsl %distroarg% -- cat - ^>^> $HOME/.ssh/authorized_keys
+ssh-keyscan -t ecdsa localhost 2>nul >> "%userprofile%\.ssh\known_hosts"
+rem wsl %distroarg% ssh-keyscan -t ecdsa localhost 2^>/dev/null>known_hosts_wsl.txt
+rem echo.>>"%userprofile%\.ssh\known_hosts"
+rem type known_hosts_wsl.txt >> "%userprofile%\.ssh\known_hosts"
+rem del known_hosts_wsl.txt
 endlocal
