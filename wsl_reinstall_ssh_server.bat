c2d5c4c24a6bbefa55b0a115a42ccdcfb053c544
JtheDroid <fabi.jobo@gmail.com>
2021-01-06 01:07:53 +0100

    Fix calling set_distro
    reinstall_ssh_server: Add restart, -y for remove

--- wsl_reinstall_ssh_server.bat
+++ wsl_reinstall_ssh_server.bat
@@ -1,3 +1,4 @@
 @echo off
-if not defined distroarg wsl_set_distro.bat
-wsl %distroarg% -u root apt update ^&^& apt remove openssh-server ^&^& apt install openssh-server -y
+if not defined distroarg call wsl_set_distro.bat
+wsl %distroarg% -u root apt update ^&^& apt remove openssh-server -y ^&^& apt install openssh-server -y
+wsl %distroarg% -u root service ssh --full-restart
--- wsl_setup_ssh_config.bat
+++ wsl_setup_ssh_config.bat
@@ -1,11 +1,12 @@
 @echo off
-if not defined distroarg wsl_set_distro.bat
+if not defined distroarg call wsl_set_distro.bat
 setlocal
 set sshdir=%userprofile%\.ssh
 if not exist "%sshdir%" mkdir "%sshdir%"
 set sshconfig="%sshdir%\config"
-set /p entryname=Enter name for ssh config entry: 
+set /p entryname=Enter name for ssh config entry (empty for wsl-x11): 
 if not defined entryname set entryname=wsl-x11
+echo.>> %sshconfig%
 echo Host %entryname%>> %sshconfig%
 echo 	Hostname localhost>> %sshconfig%
 wsl %distroarg% whoami>username.txt
